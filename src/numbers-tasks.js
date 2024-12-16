aa3cf0624c97a191d5ddb252b9f8090d7caa7dfa
programissis <vitaliatereschenko@gmail.com>
2024-12-17 03:59:50 +0500

    feat: fix function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -18,8 +18,11 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  if (typeof width !== 'number' || typeof height !== 'number') {
+    throw new Error('Both parameters must be numbers');
+  }
+  return width * height;
 }
 
 /**
@@ -33,8 +36,11 @@ function getRectangleArea(/* width, height */) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  if (typeof radius !== 'number') {
+    throw new Error('Parameter must be a number');
+  }
+  return 2 * Math.PI * radius;
 }
 
 /**
@@ -49,8 +55,12 @@ function getCircleCircumference(/* radius */) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Both parameters must be numbers');
+  }
+
+  return value1 / 2 + value2 / 2;
 }
 
 /**
@@ -68,8 +78,18 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  if (
+    typeof x1 !== 'number' ||
+    typeof y1 !== 'number' ||
+    typeof x2 !== 'number' ||
+    typeof y2 !== 'number'
+  ) {
+    throw new Error('All parameters must be numbers');
+  }
+  const dx = x2 - x1;
+  const dy = y2 - y1;
+  return Math.sqrt(dx * dx + dy * dy);
 }
 
 /**
@@ -84,8 +104,14 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Both parameters must be numbers');
+  }
+  if (a === 0) {
+    throw new Error('Coefficient a cannot be zero');
+  }
+  return -b / a;
 }
 
 /**
@@ -105,8 +131,22 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  if (
+    typeof x1 !== 'number' ||
+    typeof y1 !== 'number' ||
+    typeof x2 !== 'number' ||
+    typeof y2 !== 'number'
+  ) {
+    throw new Error('All parameters must be numbers');
+  }
+
+  const dotProduct = x1 * x2 + y1 * y2;
+  const magnitudeA = Math.sqrt(x1 * x1 + y1 * y1);
+  const magnitudeB = Math.sqrt(x2 * x2 + y2 * y2);
+
+  const cosTheta = dotProduct / (magnitudeA * magnitudeB);
+  return Math.acos(cosTheta);
 }
 
 /**
@@ -122,8 +162,11 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  if (typeof value !== 'number' || value < 0) {
+    throw new Error('Parameter must be a non-negative number');
+  }
+  return value % 10;
 }
 
 /**
@@ -137,8 +180,11 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  if (typeof value !== 'string') {
+    throw new Error('Parameter must be a string');
+  }
+  return Number(value);
 }
 
 /**
@@ -154,8 +200,12 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+    throw new Error('All parameters must be numbers');
+  }
+
+  return Math.sqrt(a * a + b * b + c * c);
 }
 
 /**
